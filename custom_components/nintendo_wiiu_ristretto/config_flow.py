6363ac188eacff6c34f70285b76f3d73259c9e49
Jordan <jordan@hrvy.uk>
2025-04-14 13:55:18 +0100

    Add DHCP discovery support and update configuration flow

--- custom_components/nintendo_wiiu_ristretto/config_flow.py
+++ custom_components/nintendo_wiiu_ristretto/config_flow.py
@@ -12,7 +12,7 @@ from homeassistant.const import (
     CONF_TIMEOUT,
 )
 
-from .const import DEFAULT_TIMEOUT, DOMAIN
+from .const import DEFAULT_TIMEOUT, DEFAULT_UPDATE_INTERVAL, DOMAIN
 
 SCHEMA = vol.Schema(
     {
@@ -29,6 +29,33 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     """Handle flow."""
 
     VERSION = 1
+    _dhcp_discovery_info = None
+
+    async def async_step_dhcp(self, discovery_info) -> ConfigFlowResult:
+        """Process initial DHCP discovery step."""
+        self._dhcp_discovery_info = discovery_info
+        return await self.async_step_dhcp_confirm()
+
+    async def async_step_dhcp_confirm(self, user_input: dict[str, Any] | None = None) -> ConfigFlowResult:
+        """DHCP Discovery configuration."""
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+        configuration = {
+            CONF_IP_ADDRESS: self._dhcp_discovery_info.ip,
+            CONF_PORT: 8572,
+            CONF_TIMEOUT: DEFAULT_TIMEOUT,
+            CONF_SCAN_INTERVAL: DEFAULT_UPDATE_INTERVAL
+        }
+        return self.async_show_form(
+            step_id="dhcp_confirm",
+            data_schema=self.add_suggested_values_to_schema(
+                data_schema=SCHEMA,
+                suggested_values=configuration
+            ),
+            description_placeholders={
+                CONF_IP_ADDRESS: self._dhcp_discovery_info.ip
+            }
+        )
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
